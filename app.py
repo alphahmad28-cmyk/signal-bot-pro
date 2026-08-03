ccea993e98c0da3e31e08f254ee36ae4cbe85202
alphahmad28-cmyk <alphahmad28@gmail.com>
2026-08-03 11:26:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 import pandas as pd
 import plotly.graph_objects as go
 import streamlit as st
-from api.market_data import fetch_candles
+from market_data import fetch_candles
 from indicators.technical import calculate_indicators
 
 # Page Configuration
